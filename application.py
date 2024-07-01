ce1474c2dad3ac2318b95ea0d9f369567b3cc936
karthick-git1 <140442362+karthick-git1@users.noreply.github.com>
2024-07-02 00:10:37 +0530

    Update application.py
    Updating content

--- application.py
+++ application.py
@@ -3,4 +3,35 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>This is my first Web App!</h1> <h2> I am updating this from my mobile </h2> </body></html>\n"
+    return "<html><head>
+	<meta name="viewport" content="width=device-width">
+	<link href="styles/style.css" rel="stylesheet">
+</head>
+
+
+	<body>
+		<meta charset="utf-8">
+		<h1>Hello! This is my first Web App!</h1> 
+			<h2 style="
+    color: chocolate;
+"> I am updating this from my mobile </h2> 
+
+<p style="
+    COLOR: darkblue;
+">  This website is designed for my son Tholkaapiyan </p>
+
+<img src="images/IMG_9714.jpg" alt="My test image" style="max-width: 50%;
+    
+">
+
+<ul>
+  <li>Monthly_birthdays</li>
+  <li>Picnics</li>
+  <li>With_parents</li>
+</ul>
+
+<p>Enjoy…</p>
+
+
+	
+</body></html>	\n"
