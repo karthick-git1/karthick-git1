3dbe00c294c8eff8a3572eb25067b8f04decadbb
karthick-git1 <140442362+karthick-git1@users.noreply.github.com>
2024-07-01 08:24:44 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<html><body><h1>This is my first Web App!</h1></body></html>\n"
+    return "<html><body><h1>This is my first Web App!</h1> <h2> I am updating this from my mobile </h2> </body></html>\n"
